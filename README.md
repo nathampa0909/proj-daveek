b750a280b46e5b239ab5cdc84840273637b90b55
lokecross <lucas.marcelino.silva@live.com>
2019-08-12 12:23:56 -0300

    pages

---
+++ README.md
@@ -0,0 +1,9 @@
+# Projeto Daveek
+
+Projeto feito por trainees para treinamento de HTML/CSS/BOOTSTRAP.
+
+start: 
+npm i -g http-server
+http-server .
+
+link: localhost:8080/index.html
---
+++ script.js
@@ -0,0 +1,10 @@
+document.querySelectorAll('[nav-link]').forEach(link => {
+    const conteudo = document.getElementById('conteudo')
+
+    link.onclick = function(e) {
+        e.preventDefault()
+        fetch(link.href)
+            .then(resp => resp.text())
+            .then(html => conteudo.innerHTML = html)
+    }
+})
